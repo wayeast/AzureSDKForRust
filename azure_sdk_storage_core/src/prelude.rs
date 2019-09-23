c2d7bcfe0ec3302ac54b8f8f6330bf17c91c545a
Huston Bokinsky <huston@deepgram.com>
2019-09-23 11:18:13 -0700

    Working if unsatisfactory implementation

--- azure_sdk_storage_core/src/prelude.rs
+++ azure_sdk_storage_core/src/prelude.rs
@@ -1,1 +1,13 @@
 pub use crate::client::Client;
+
+pub use crate::SharedAccessSignatureSupport;
+
+pub use crate::shared_access_signature::{
+    ClientSharedAccessSignature,
+    SasPermissions,
+    SasProtocol,
+    SasResource,
+    SasResourceType,
+    SasService,
+    SasVersion,
+};
